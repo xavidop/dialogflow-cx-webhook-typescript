87f91c8c1aed0dca0926f7e6e8749a54aa79c455
xavidop <xavi_tb@hotmail.com>
2023-10-16 10:39:07 +0200

    fix: modify CX types

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 /* eslint-disable max-len */
 import {http, Request, Response} from "@google-cloud/functions-framework";
-import * as dialogflow from "@google-cloud/dialogflow-cx";
+import {protos as dftypes} from "@google-cloud/dialogflow-cx";
 
 http("HandleWebhookRequest", (req: Request, res: Response) => {
-  type WebhookRequest = dialogflow.protos.google.cloud.dialogflow.cx.v3beta1.WebhookRequest;
-  type WebhookResponse = dialogflow.protos.google.cloud.dialogflow.cx.v3beta1.WebhookResponse;
+  type WebhookRequest = dftypes.google.cloud.dialogflow.cx.v3beta1.WebhookRequest;
+  type WebhookResponse = dftypes.google.cloud.dialogflow.cx.v3beta1.WebhookResponse;
 
-  const body = <WebhookRequest><unknown>req.body;
+  const body = <WebhookRequest>req.body;
   console.log(body.languageCode);
 
-  const response: WebhookResponse = new dialogflow.protos.google.cloud.dialogflow.cx.v3beta1.WebhookResponse();
+  const response: WebhookResponse = new dftypes.google.cloud.dialogflow.cx.v3beta1.WebhookResponse();
 
   response.fulfillmentResponse = {
     messages: [{
